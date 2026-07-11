3cd992201967f5003cb96f6bf42ce6e32b316ced
Kakashi <rathodsahil18852@gmail.com>
2026-07-11 16:38:06 +0530

    Simple Program for Encapsulation

--- Encapsulation.py
+++ Encapsulation.py
@@ -1,11 +1,12 @@
 class Student:
-    def __init__(self,name,age,password):
+    def __init__(self,name,age,gpa,password):
         self.name = name
-        self._age = age
+        self.age = age
+        self._gpa = gpa
         self.__password = password
         
     def get_password(self):
         return self.__password
 
-std1 = Student("ABC","20","EnterAnyPassword")
-print(std1.name,std1._age,std1.get_password())
+std1 = Student("ABC","20",9.0,"EnterAnyPassword")
+print(std1.name,std1._age,std1.gpa,std1.get_password())
