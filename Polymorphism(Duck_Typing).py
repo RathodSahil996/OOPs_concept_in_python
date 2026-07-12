90c0c07dfe7bde01af025be067c9cd8c05e19be7
Kakashi <rathodsahil18852@gmail.com>
2026-07-12 16:48:54 +0530

    Duck Typing

---
+++ Polymorphism(Duck_Typing).py
@@ -0,0 +1,13 @@
+class teacher:
+    def get_designation(self):
+        print("Designation = Teacher")
+        
+class Accountant:    
+    def get_designation(self):
+        print("Designation = Accountant")
+        
+        
+d1 = teacher()
+d1.get_designation()
+d2 = Accountant()
+d2.get_designation()
